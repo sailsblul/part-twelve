28b15890cd212241a7a5486e716c7fb950cc5411
Silas Bull <BullSila402@GoTVDSB.ca>
2022-04-21 11:02:00 -0400

    Add project files.

---
+++ part twelve/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace part_twelve
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.lstMovies = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.ofdMovie = new System.Windows.Forms.OpenFileDialog();
+            this.sfdMovie = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(494, 166);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(413, 166);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(413, 195);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(494, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(413, 224);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Location = new System.Drawing.Point(410, 88);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(90, 13);
+            this.lblEnter.TabIndex = 5;
+            this.lblEnter.Text = "Enter Movie Title:";
+            // 
+            // lstMovies
+            // 
+            this.lstMovies.FormattingEnabled = true;
+            this.lstMovies.Location = new System.Drawing.Point(273, 12);
+            this.lstMovies.Name = "lstMovies";
+            this.lstMovies.Size = new System.Drawing.Size(120, 251);
+            this.lstMovies.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(413, 104);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(156, 20);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // ofdMovie
+            // 
+            this.ofdMovie.FileName = "movies";
+            this.ofdMovie.Filter = "\"Text files (*.txt)|*.txt";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(583, 286);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstMovies);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "MainForm";
+            this.Text = "Reading and Wrinting FIles";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.ListBox lstMovies;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.OpenFileDialog ofdMovie;
+        private System.Windows.Forms.SaveFileDialog sfdMovie;
+    }
+}
+
---
+++ part twelve/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace part_twelve
+{
+    public partial class MainForm : Form
+    {
+        List<string> movies = new List<string>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string film = txtTitle.Text.Trim();
+            if (!String.IsNullOrEmpty(film) && !movies.Contains(film))
+                movies.Add(film);
+            lstMovies.DataSource = null;
+            lstMovies.DataSource = movies;
+            txtTitle.Clear();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstMovies.SelectedIndex >= 0)
+                movies.Remove((string)lstMovies.SelectedItem);
+            lstMovies.DataSource = null;
+            lstMovies.DataSource = movies;
+            txtTitle.Clear();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            movies.Sort();
+            lstMovies.DataSource = null;
+            lstMovies.DataSource = movies;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (ofdMovie.ShowDialog() == DialogResult.OK)
+            {
+                movies.Clear();
+                foreach (string line in File.ReadLines(ofdMovie.FileName))
+                    movies.Add(line);
+                lstMovies.DataSource = null;
+                lstMovies.DataSource = movies;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sfdMovie.ShowDialog() == DialogResult.OK)
+            {
+                string file = sfdMovie.FileName;
+                if (file.Substring(file.Length - 4) != ".txt")
+                    file += ".txt";
+                StreamWriter writer = new StreamWriter(file);
+                foreach (string movie in movies)
+                    writer.WriteLine(movie);
+                writer.Close();
+            }
+        }
+    }
+}
